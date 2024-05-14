c8d5916018d733fc54883e3cd6e35fd6a2013ed0
ShipraNigotiya <shipranigotiya@gmail.com>
2024-05-14 22:50:47 +0530

    commit

--- frontend/src/app/admin/dashboard/page.jsx
+++ frontend/src/app/admin/dashboard/page.jsx
@@ -317,7 +317,7 @@ const AdminDashboard = () => {
         <span className="mx-4 font-medium">Settings</span>
       </a>
     </nav>
-    <a href="#" className="flex items-center px-4 -mx-2">
+    <a href="#" className="flex items-center px-4 -mx-2"> 
       <img
         className="object-cover mx-2 rounded-full h-9 w-9"
         src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
